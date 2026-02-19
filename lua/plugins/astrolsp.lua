b0de339f7ec4f2dba4fef056af5cd7ee1179868f
Víctor Cuadrado Juan <me@viccuad.me>
2026-02-19 10:28:11 +0100

    disable autoload of harper-ls

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -72,6 +72,7 @@ return {
       -- the key is the server that is being setup with `lspconfig`
       -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
       -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
+      harper_ls = false,
     },
     -- Configure buffer local auto commands to add when attaching a language server
     autocmds = {
